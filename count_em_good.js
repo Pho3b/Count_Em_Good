169052f7baef8b33ec4c7c1454f655c2544527d4
Pho3b <andrea.rinaldi54@gmail.com>
2019-02-01 14:23:49 +0100

    Now Checking for null elements

--- count_em_good.js
+++ count_em_good.js
@@ -7,32 +7,49 @@
 	//Funzione che conta quanti caratteri possono ancora esseri inseriti nei campi titolo e testo della creazione e modifica notifica
 	function count_em_good(trigger_id,max_char,counter_id,on_load){
 		var max_char = max_char;
-		var text_field_len = document.getElementById(trigger_id).value.length;
-		var diff = max_char - text_field_len;
-		var element = document.getElementById(counter_id);
-		
-		
-		//This piece of code runs only on load
-		if(on_load){
-			var element_par = element.previousSibling.innerHTML = 'Max Characters ' + max_char + ' : ';
-			element.innerHTML = max_char;
-			element.style.color = "#5cb85c";
-
-			first_load = false;
+		//Taking the elements
+		try{
+			var text_field_len = document.getElementById(trigger_id).value.length;
+		}catch(e){
+			console.log("Didn't find any element with this ID: " + trigger_id);
+			var text_field_len = 0;
 		}
-	
-	
-		if(diff >= 0){
-			if(diff > max_char/2){
+		
+		try{
+			var element = document.getElementById(counter_id);
+		}catch(e){
+			var element = null;
+		}
+		
+		
+		var diff = max_char - text_field_len;
+
+		
+		if(element!= null){
+			//Questo pezzo di codice parte solo al caricamento
+			if(on_load){
+				var element_par = element.previousSibling.innerHTML = 'Max Characters ' + max_char + ' : ';
+				element.innerHTML = max_char;
 				element.style.color = "#5cb85c";
-			}else if(diff <= ((max_char/2) / 2)){
-				element.style.color = "#d9534f";
-			}else if(diff <= max_char/2){
-				element.style.color = "#f0ad4e";
+
+				first_load = false;
 			}
-			element.innerHTML = diff;
+		
+		
+			if(diff >= 0){
+				if(diff > max_char/2){
+					element.style.color = "#5cb85c";
+				}else if(diff <= ((max_char/2) / 2)){
+					element.style.color = "#d9534f";
+				}else if(diff <= max_char/2){
+					element.style.color = "#f0ad4e";
+				}
+				element.innerHTML = diff;
+			}else{
+				var text_field_value = document.getElementById(trigger_id).value;
+				document.getElementById(trigger_id).value = text_field_value.substring(text_field_value,max_char);
+			}	
 		}else{
-			var text_field_value = document.getElementById(trigger_id).value;
-			document.getElementById(trigger_id).value = text_field_value.substring(text_field_value,max_char);
-		}	
+			console.log("No Elements found");
+		}
 	}
